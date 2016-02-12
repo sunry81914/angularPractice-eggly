2e532786b596e67c366dd14ad96dad332ecf9498
Bella Wang <sunry81914@gmail.com>
2016-02-13 01:01:21 +0800

    express : setting router localhost:3000 is homeURL

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res){
-	res.send('Hello World!');
-});
-
 app.listen(3000, function(){
 	console.log('example app listening on port 3000');
 });
 
-app.use('/static', express.static(__dirname + '/app'));
+app.use('/', express.static(__dirname + '/app'));
