ac33a9020307a151ce57099740310079ffe2a08a
Bella Wang <sunry81914@gmail.com>
2016-02-13 01:33:16 +0800

    eggly:  toggle between the editing and creating bookmark states

--- app/js/eggly-app.js
+++ app/js/eggly-app.js
@@ -26,6 +26,10 @@ angular.module('Eggly', [
 
 		function setCurrentCategory(category) {
 			$scope.currentCategory = category;
+
+			cancelCreating();
+			cancelEditing();
+			
 			console.log('currentCategory is ' + $scope.currentCategory );
 		}
 
@@ -36,6 +40,44 @@ angular.module('Eggly', [
 		$scope.setCurrentCategory = setCurrentCategory;
 		$scope.isCurrentCategory = isCurrentCategory;
 
+		//----------------------------------------------
+		// CREATING AND EDITING STATES
+		//----------------------------------------------
+		$scope.isCreating = false;
+		$scope.isEditing = false;
+
+		function startCreating() {
+			$scope.isCreating = true;
+			$scope.isEditing = false;
+		}
+
+		function cancelCreating() {
+			$scope.isCreating = false;
+		}
+
+		function startEditing() {
+			$scope.isCreating = false;
+			$scope.isEditing = true;
+		}
+
+		function cancelEditing() {
+			$scope.isEditing = false;
+		}
+
+		function shouldShowCreating() {
+			return $scope.currentCategory && !$scope.isEditing;
+		}
+
+		function shouldShowEditing() {
+			return $scope.isEditing && !$scope.isCreating;
+		}
+
+		$scope.startCreating = startCreating;
+		$scope.cancelCreating = cancelCreating;
+		$scope.startEditing = startEditing;
+		$scope.cancelEditing = cancelEditing;
+		$scope.shouldShowCreating = shouldShowCreating;
+		$scope.shouldShowEditing = shouldShowEditing;
 
 	})
 ;
